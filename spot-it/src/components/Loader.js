ba301dbeb3904d5627b9173e33584ec44725495d
meetkavad <meet.kavad07@gmail.com>
2025-02-09 16:35:34 +0530

    loader changes

--- spot-it/src/components/Loader.js
+++ spot-it/src/components/Loader.js
@@ -4,7 +4,7 @@ import "./Loader.css";
 const Loader = () => {
   return (
     <div className="loader-overlay">
-      <div className="loader"></div>
+      <div className="loader"> </div>
     </div>
   );
 };
